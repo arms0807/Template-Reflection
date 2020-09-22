5f39d940cedd7f10d5e4e340a55bc0598199aba8
arms0807 <arms0807@hotmail.com>
2020-09-21 20:29:44 -0400

    Initialize project and create Models

---
+++ Template.Reflection/Template.Reflection.Main/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Template.Reflection.Main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Template.Reflection/Template.Reflection.Model/BaseModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Template.Reflection.Model
+{
+    public class BaseModel
+    {
+        public int Id { get; set; }
+    }
+}
